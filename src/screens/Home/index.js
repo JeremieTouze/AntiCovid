d7171e13e17fc52f76a484b06e39c4846f5dea6d
unknown <jeremie.touze@gmail.com>
2020-03-20 15:13:24 +0100

    clean comments

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,29 +1,10 @@
 import Page from "@components/Page";
-import styled from "styled-components";
 import HomeImage from "./Image";
 import HomeDescription from "./Description";
 import HomeMobile from "./HomeMobile";
-import useWindowSize from "@hooks/useWindowSize";
 import { Breakpoint, BreakpointProvider } from 'react-socks';
 
 const Home = () => {
-    // const size = useWindowSize();
-    // if (size.width > 768) {
-    //     dc = (
-    //         <React.Fragment>
-    //             <HomeImage />
-    //             <HomeDescription />
-    //         </React.Fragment>
-    //     )
-    // } else {
-    //     dc = (
-    //         <React.Fragment>
-    //             <HomeImage />
-    //             <HomeDescription />
-    //         </React.Fragment>
-    //     )
-    // }
-
     return (
         <Page title="Accueil">
             <BreakpointProvider>
